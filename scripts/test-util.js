f480d5ebe4b6f71dc6f9540a90bec1e44d367073
dryoma <dryoma@users.noreply.github.com>
2016-10-09 00:09:26 +0800

    Fix missing environment in test scripts
    Was causing the scripts to fail on Linux

--- scripts/test-util.js
+++ scripts/test-util.js
@@ -2,14 +2,14 @@
 // So that we could just run "npm run test-util findCommentsInRaws"
 
 const exec = require("child_process").exec
+const env = process.env
+env.FORCE_COLOR = 1
 
 process.argv.slice(2).forEach(function (arg) {
   const cmd = "\"./node_modules/.bin/tape\" -r babel-register \"src/utils/__tests__/" + arg + ".js\" | \"./node_modules/.bin/tap-spec\""
   const child = exec(
     cmd,
-    { env: { FORCE_COLOR: "always" } }
-    // We actually don't need this callback now
-    // function (error, stdout, stderr) {}
+    env
   )
 
   child.stdout.pipe(process.stdout)
