90fcd445d74ae2573978cbf33cfe84622615f061
lithiumlron <edwardtu.dev@gmail.com>
2017-07-05 16:32:52 -0700

    Add rule: at-rule-no-unknown

---
+++ src/rules/at-rule-no-unknown/index.js
@@ -0,0 +1,59 @@
+import { rules, utils } from "stylelint"
+import { namespace } from "../../utils"
+
+const sassAtRules = [
+  "at-root",
+  "content",
+  "debug",
+  "each",
+  "else",
+  "else if",
+  "error",
+  "extend",
+  "for",
+  "function",
+  "if",
+  "import",
+  "include",
+  "media",
+  "mixin",
+  "return",
+  "warn",
+  "while",
+]
+
+const ruleToCheckAgainst = "at-rule-no-unknown"
+
+const ruleName = namespace(ruleToCheckAgainst)
+
+export const messages = utils.ruleMessages(ruleName, {
+  rejected: rules[ruleToCheckAgainst].messages.rejected,
+})
+
+export default function (primaryOption, secondaryOptions) {
+  return (root, result) => {
+    const optionsAtRules = secondaryOptions && secondaryOptions.ignoreAtRules
+    const ignoreAtRules = sassAtRules.concat(optionsAtRules || [])
+    const defaultedOptions = Object.assign({}, secondaryOptions, { ignoreAtRules })
+
+    utils.checkAgainstRule({
+      ruleName: ruleToCheckAgainst,
+      ruleSettings: [ primaryOption, defaultedOptions ],
+      root,
+    }, (warning) => {
+      root.walkAtRules((atRule) => {
+        const name = atRule.name
+        if (!ignoreAtRules.includes(name)) {
+          utils.report({
+            message: messages.rejected(`@${name}`),
+            ruleName,
+            result,
+            node: warning.node,
+            line: warning.line,
+            column: warning.column,
+          })
+        }
+      })
+    })
+  }
+}
