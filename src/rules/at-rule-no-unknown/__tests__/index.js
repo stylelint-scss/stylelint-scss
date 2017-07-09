b2fe8d5c2dad8523f9c70a17efdda2c28bb3cda3
Krister Kari <kristerkari@users.noreply.github.com>
2017-07-09 13:47:48 +0300

    at-rule-no-unknown: copy tests from stylelint (#131)
    * at-rule-no-unknown: copy tests from stylelint
    
    * Add a test to make sure that "@else if" works

--- src/rules/at-rule-no-unknown/__tests__/index.js
+++ src/rules/at-rule-no-unknown/__tests__/index.js
@@ -7,6 +7,54 @@ testRule(rule, {
   syntax: "scss",
 
   accept: [ {
+    code: "@charset 'UTF-8';",
+  }, {
+    code: "@CHARSET 'UTF-8';",
+  }, {
+    code: "@charset 'iso-8859-15'",
+  }, {
+    code: "@import url(\"fineprint.css\") print;",
+  }, {
+    code: "@import 'custom.css'",
+  }, {
+    code: "@import url('landscape.css') screen and (orientation:landscape);",
+  }, {
+    code: "@namespace url(http://www.w3.org/1999/xhtml);",
+  }, {
+    code: "@namespace prefix url(XML-namespace-URL);",
+  }, {
+    code: "@media print { body { font-size: 10pt } }",
+  }, {
+    code: "@media (max-width: 960px) { body { font-size: 13px } }",
+  }, {
+    code: "@media screen, print { body { line-height: 1.2 } }",
+  }, {
+    code: "@supports (--foo: green) { body { color: green; } }",
+  }, {
+    code: "@supports ( (perspective: 10px) or (-webkit-perspective: 10px) ) { font-size: 10pt }",
+  }, {
+    code: "@counter-style win-list { system: fixed; symbols: url(gold-medal.svg); suffix: ' ';}",
+  }, {
+    code: "@document url(http://www.w3.org/), url-prefix(http://www.w3.org/Style/), domain(mozilla.org), regexp('https:.*')",
+  }, {
+    code: "@page :left { margin-left: 4cm; }",
+  }, {
+    code: "@font-face { font-family: MyHelvetica; src: local(\"Helvetica\"), url(MgOpenModern.ttf); }",
+  }, {
+    code: "@keyframes identifier { 0% { top: 0; left: 0; } 30% { top: 50px; } 68%, 100% { top: 100px; left: 100%; } }",
+  }, {
+    code: "@-webkit-keyframes identifier { 0% { top: 0; left: 0; } 30% { top: 50px; } 68%, 100% { top: 100px; left: 100%; } }",
+  }, {
+    code: "@viewport { min-width: 640px; max-width: 800px; }",
+  }, {
+    code: "@viewport { orientation: landscape; }",
+  }, {
+    code: "@counter-style winners-list { system: fixed; symbols: url(gold-medal.svg); suffix: \" \"; }",
+  }, {
+    code: "@font-feature-values Font One { @styleset { nice-style: 12; } }",
+  }, {
+    code: ".foo { color: red; @nest .parent & { color: blue; } }",
+  }, {
     code: "@function foo () {}",
   }, {
     code: "@Function foo () { @return 1; }",
@@ -18,6 +66,8 @@ testRule(rule, {
     code: "@if ($i) {}",
   }, {
     code: "@if ($i) {} @else {}",
+  }, {
+    code: "@if ($i) {} @else if {} @else {}",
   } ],
 
   reject: [{
