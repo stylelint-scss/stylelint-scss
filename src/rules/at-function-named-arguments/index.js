84eaf437917baf2e32cd25780d613dcf95c152a3
Diego Hernandez <Diego.Hernandez@ibm.com>
2017-11-30 10:46:45 -0600

    fix(at-function-named-arguments): Remove "always-multiple-arguments" from valid options

--- src/rules/at-function-named-arguments/index.js
+++ src/rules/at-function-named-arguments/index.js
@@ -30,7 +30,7 @@ export default function(expectation, options) {
       ruleName,
       {
         actual: expectation,
-        possible: ["always", "never", "always-multiple-arguments"]
+        possible: ["always", "never"]
       },
       {
         actual: options,
