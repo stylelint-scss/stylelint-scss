2564023f2f888bbefaccec872fc4c2f87b2991a3
freezy <marek.viger@gmail.com>
2018-02-03 15:04:47 +0100

    More tests

--- src/rules/dollar-variable-default/__tests__/index.js
+++ src/rules/dollar-variable-default/__tests__/index.js
@@ -7,12 +7,20 @@ testRule(rule, {
 
   accept: [
     {
+      code: "a { color: blue }",
+      description: "Basic style definition"
+    },
+    {
       code: "$var: 10px !default",
       description: "Global variable with !default"
     },
     {
       code: "a { $var: 10px !default }",
       description: "Local variable with !default"
+    },
+    {
+      code: ".class { a { $var: 10px !default } }",
+      description: "Nested local variable with !default"
     }
   ],
 
@@ -26,6 +34,11 @@ testRule(rule, {
       code: "a { $var: 10px }",
       message: messages.expected("$var"),
       description: "Local variable without !default"
+    },
+    {
+      code: ".class { a { $var: 10px } }",
+      message: messages.expected("$var"),
+      description: "Nested local variable without !default"
     }
   ]
 });
@@ -38,12 +51,28 @@ testRule(rule, {
 
   accept: [
     {
+      code: "a { color: blue }",
+      description: "Basic style definition (ignoring local variables)"
+    },
+    {
+      code: "$var: 10px !default",
+      description: "Global variable with !default (ignoring local variables)"
+    },
+    {
       code: "a { $var: 10px !default }",
       description: "Ignore local variable with !default"
     },
     {
+      code: ".class { a { $var: 10px !default } }",
+      description: "Ignore nested local variable with !default"
+    },
+    {
       code: "a { $var: 10px }",
       description: "Ignore local variable without !default"
+    },
+    {
+      code: ".class { a { $var: 10px } }",
+      description: "Ignore nested local variable without !default"
     }
   ],
 
