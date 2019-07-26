c24d4a33ede3d37a30c1573b54d4661bcf47ab04
Alex Stephen <alexstephen@google.com>
2019-07-26 14:31:20 -0700

    time to test

--- src/rules/at-conditional-rule-no-parentheses/index.js
+++ src/rules/at-conditional-rule-no-parentheses/index.js
@@ -1,5 +1,4 @@
-import { isRegExp, isString } from "lodash";
-import { rules, utils } from "stylelint";
+import { utils } from "stylelint";
 import { namespace } from "../../utils";
 
 export const ruleName = namespace("at-conditional-rule-no-parentheses");
@@ -8,6 +7,32 @@ export const messages = utils.ruleMessages(ruleName, {
   rejected: "Do not use () to surround statements for @-rules"
 });
 
-export default function(primaryOption, secondaryOptions) {
-  return (root, result) => {};
+const conditional_rules = ["if", "while", "else if"];
+
+export default function(primary) {
+  return (root, result) => {
+    const validOptions = utils.validateOptions(result, ruleName, {
+      actual: primary
+    });
+
+    if (!validOptions) {
+      return;
+    }
+
+    root.walkAtRules(atrule => {
+      // Check if this is a conditional rule.
+      if (!conditional_rules.includes(atrule.name)) {
+        return;
+      }
+
+      if (atrule.params.match(/ ?\(.*\) ?/)) {
+        utils.report({
+          message: messages.rejected,
+          node: atrule,
+          result,
+          ruleName
+        });
+      }
+    });
+  };
 }
