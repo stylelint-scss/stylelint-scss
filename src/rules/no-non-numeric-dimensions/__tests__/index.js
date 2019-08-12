27771dab941ea952d3beef1320ff2f1a69e7d380
Alex Stephen <alexstephen@google.com>
2019-08-12 14:28:29 -0700

    tests pass

--- src/rules/no-non-numeric-dimensions/__tests__/index.js
+++ src/rules/no-non-numeric-dimensions/__tests__/index.js
@@ -1,4 +1,4 @@
-import rule, { ruleName, messages } from "..";
+import rule, { ruleName, messages, units } from "..";
 
 testRule(rule, {
   ruleName,
@@ -7,22 +7,49 @@ testRule(rule, {
   accept: [
     {
       code: `
-      p {
-        padding: "1" * 1px;
-      }
-      `,
-      description: "Accepts proper value interpolation"
+    p {
+      padding: "1" * 1em;
+    }
+    `,
+      description: "Accepts proper value interpolation with em"
     }
   ],
   reject: [
     {
       code: `
       p {
-        padding: #{value}px;
+        padding: #{value}em;
       }
       `,
       messages: messages.rejected,
-      description: "Rejects interpolation with a unit"
+      description: "Rejects interpolation with em"
     }
-  ]
+  ],
+  acceptBlah: loopOverUnits({
+    code: `
+    p {
+      padding: "1" * 1%unit%;
+    }
+    `,
+    description: "Accepts proper value interpolation with %unit%"
+  }),
+  rejectBlah: loopOverUnits({
+    code: `
+      p {
+        padding: #{value}%unit%;
+      }
+      `,
+    messages: messages.rejected,
+    description: "Rejects interpolation with %unit%"
+  })
 });
+
+function loopOverUnits(codeBlock) {
+  units.map(unit => {
+    return {
+      code: codeBlock.code.replace("%unit%", unit),
+      description: codeBlock.description.replace("%unit%", unit),
+      messages: codeBlock.messages
+    };
+  });
+}
--- src/rules/no-non-numeric-dimensions/index.js
+++ src/rules/no-non-numeric-dimensions/index.js
@@ -1,14 +1,78 @@
 import { utils } from "stylelint";
-import { namespace, isNativeCssFunction } from "../../utils";
+import { namespace } from "../../utils";
 import valueParser from "postcss-value-parser";
 
-export const ruleName = namespace("function-quote-no-quoted-strings-inside");
+export const ruleName = namespace("no-non-numeric-dimensions");
 
 export const messages = utils.ruleMessages(ruleName, {
   rejected: "Expected `$value * 1px` instead of `#{value}px"
 });
 
-function rule(primary, _, context) {
+export const units = [
+  // Font-relative lengths:
+  // https://www.w3.org/TR/css-values-4/#font-relative-lengths
+  "em",
+  "ex",
+  "cap",
+  "ch",
+  "ic",
+  "rem",
+  "lh",
+  "rlh",
+
+  // Viewport-relative lengths:
+  // https://www.w3.org/TR/css-values-4/#viewport-relative-lengths
+  "vw",
+  "vh",
+  "vi",
+  "vb",
+  "vmin",
+  "vmax",
+
+  // Absolute lengths:
+  // https://www.w3.org/TR/css-values-4/#absolute-lengths
+  "cm",
+  "mm",
+  "Q",
+  "in",
+  "pc",
+  "pt",
+  "px",
+
+  // Angle units:
+  // https://www.w3.org/TR/css-values-4/#angles
+  "deg",
+  "grad",
+  "rad",
+  "turn",
+
+  // Duration units:
+  // https://www.w3.org/TR/css-values-4/#time
+  "s",
+  "ms",
+
+  // Frequency units:
+  // https://www.w3.org/TR/css-values-4/#frequency
+  "Hz",
+  "kHz",
+
+  // Resolution units:
+  // https://www.w3.org/TR/css-values-4/#resolution
+  "dpi",
+  "dpcm",
+  "dppx",
+  "x",
+
+  // Flexible lengths:
+  // https://www.w3.org/TR/css-grid-1/#fr-unit
+  "fr",
+
+  // Percentage:
+  // https://www.w3.org/TR/css-values-4/#percentages
+  "%"
+];
+
+export default function rule(primary) {
   return (root, result) => {
     const validOptions = utils.validateOptions(result, ruleName, {
       actual: primary
@@ -17,7 +81,40 @@ function rule(primary, _, context) {
     if (!validOptions) {
       return;
     }
+
+    root.walkDecls(decl => {
+      valueParser(decl.value).walk(node => {
+        // All words are non-quoted, while strings are quoted.
+        // If quoted, it's probably a deliberate non-numeric dimension.
+        if (node.type !== "word") {
+          return;
+        }
+
+        if (!isInterpolated(node.value)) {
+          return;
+        }
+
+        utils.report({
+          ruleName,
+          result,
+          message: messages.rejected,
+          node: decl
+        });
+      });
+    });
   };
 }
 
-export default rule;
+function isInterpolated(value) {
+  let boolean = false;
+
+  units.forEach(unit => {
+    const regex = new RegExp("#{[a-z_-]*}" + unit);
+
+    if (value.match(regex)) {
+      boolean = true;
+    }
+  });
+
+  return boolean;
+}
