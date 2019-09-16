087301446fe274c500907edb2269c0856f859618
Paul Gascou-Vaillancourt <paul.gascvail@gmail.com>
2019-09-16 15:56:33 -0400

    Add no-duplicate-mixin rule

---
+++ src/rules/no-duplicate-mixins/__tests__/index.js
@@ -0,0 +1,110 @@
+import rule, { ruleName, messages } from "..";
+
+testRule(rule, {
+  ruleName,
+  config: [true],
+  syntax: "scss",
+
+  accept: [
+    {
+      code: `
+      @mixin font-size-default {
+        font-size: 16px;
+      }
+    `,
+      description: "A single mixin."
+    },
+    {
+      code: `
+      @mixin font-size-default {
+        font-size: 16px;
+      }
+      @mixin font-size-lg {
+        font-size: 18px;
+      }
+    `,
+      description: "Two mixins with different names."
+    }
+  ],
+
+  reject: [
+    {
+      code: `
+      @mixin font-size-default {
+        font-size: 16px;
+      }
+      @mixin font-size-default {
+        font-size: 18px;
+      }
+    `,
+      column: 7,
+      line: 5,
+      message: messages.rejected("font-size-default"),
+      description: "Two mixins with the same names."
+    },
+    {
+      code: `
+      @mixin font-size-default {
+        font-size: 16px;
+      }
+      @mixin font-size-sm {
+        font-size: 14px;
+      }
+      @mixin font-size-default {
+        font-size: 18px;
+      }
+    `,
+      column: 7,
+      line: 8,
+      message: messages.rejected("font-size-default"),
+      description: "Three mixins including two with the same names."
+    },
+    {
+      code: `
+      @mixin font-size {
+        font-size: 16px;
+      }
+      @mixin font-size($var) {
+        font-size: $var;
+      }
+    `,
+      column: 7,
+      line: 5,
+      message: messages.rejected("font-size"),
+      description:
+        "Two mixins with the same names including one accepting arguments."
+    },
+    {
+      code: `
+      @mixin font-size($property, $value) {
+        #{$property}: $value;
+      }
+      @mixin font-size($var) {
+        font-size: $var;
+      }
+    `,
+      column: 7,
+      line: 5,
+      message: messages.rejected("font-size"),
+      description: "Two mixins with the same names accepting arguments."
+    },
+    {
+      code: `
+      @mixin font-size {
+        color: blue;
+      }
+
+      .b {
+        @mixin font-size {
+          color: red;
+        }
+        @include font-size;
+      }
+    `,
+      column: 9,
+      line: 7,
+      message: messages.rejected("font-size"),
+      description: "Two mixins with the same names accepting arguments."
+    }
+  ]
+});
--- src/utils/index.js
+++ src/utils/index.js
@@ -1,4 +1,5 @@
 export { addEmptyLineBefore } from "./addEmptyLineBefore";
+export { default as atRuleBaseName } from "./atRuleBaseName";
 export { default as atRuleParamIndex } from "./atRuleParamIndex";
 export { default as beforeBlockString } from "./beforeBlockString";
 export { default as blockString } from "./blockString";
@@ -8,17 +9,13 @@ export { default as eachRoot } from "./eachRoot";
 export { default as findCommentsInRaws } from "./findCommentsInRaws";
 export { default as hasBlock } from "./hasBlock";
 export { default as hasEmptyLine } from "./hasEmptyLine";
-export {
-  default as hasInterpolatingAmpersand
-} from "./hasInterpolatingAmpersand";
+export { default as hasInterpolatingAmpersand } from "./hasInterpolatingAmpersand";
 export { default as isInlineComment } from "./isInlineComment";
 export { default as isNativeCssFunction } from "./isNativeCssFunction";
 export { default as isSingleLineString } from "./isSingleLineString";
 export { default as isStandardRule } from "./isStandardRule";
 export { default as isStandardSelector } from "./isStandardSelector";
-export {
-  default as isStandardSyntaxProperty
-} from "./isStandardSyntaxProperty";
+export { default as isStandardSyntaxProperty } from "./isStandardSyntaxProperty";
 export { default as isWhitespace } from "./isWhitespace";
 export { default as namespace } from "./namespace";
 export { default as optionsHaveException } from "./optionsHaveException";
