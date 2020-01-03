ee7fb902ec777ad3c6034890afb4a44c7feba1b6
Alex Stephen <alexstephen@google.com>
2020-01-03 13:25:02 -0800

    long list of rules

---
+++ src/rules/no-global-function-names/index.js
@@ -0,0 +1,138 @@
+rules = {
+  red: "color",
+  blue: "color",
+  green: "color",
+  mix: "color",
+  hue: "color",
+  saturation: "color",
+  lightness: "color",
+  complement: "color",
+  invert: "color",
+  alpha: "color",
+  "adjust-color": "color",
+  "scale-color": "color",
+  "change-color": "color",
+  "ie-hex-str": "color",
+  "map-get": "map",
+  "map-merge": "map",
+  "map-remove": "map",
+  "map-keys": "map",
+  "map-values": "map",
+  "map-has-key": "map",
+  unquote: "string",
+  quote: "string",
+  "str-length": "string",
+  "str-insert": "string",
+  "str-index": "string",
+  "str-slice": "string",
+  "to-upper-case": "string",
+  "to-lower-case": "string",
+  "unique-id": "string",
+  percentage: "math",
+  round: "math",
+  ceil: "math",
+  floor: "math",
+  abs: "math",
+  min: "math",
+  max: "math",
+  random: "math",
+  unit: "math",
+  unitless: "math",
+  comparable: "math",
+  length: "list",
+  nth: "list",
+  "set-nth": "list",
+  join: "list",
+  append: "list",
+  zip: "list",
+  index: "list",
+  "list-separator": "list",
+  "feature-exists": "meta",
+  "variable-exists": "meta",
+  "global-variable-exists": "meta",
+  "function-exists": "meta",
+  "mixin-exists": "meta",
+  inspect: "meta",
+  "get-function": "meta",
+  "type-of": "meta",
+  call: "meta",
+  "content-exists": "meta",
+  keywords: "meta",
+  "selector-nest": "selector",
+  "selector-append": "selector",
+  "selector-replace": "selector",
+  "selector-unify": "selector",
+  "is-superselector": "selector",
+  "simple-selectors": "selector",
+  "selector-parse": "selector",
+  "selector-extend": "selector"
+};
+
+new_rule_names = {
+  "adjust-color": "adjust",
+  "scale-color": "scale",
+  "change-color": "change",
+  "map-get": "get",
+  "map-merge": "merge",
+  "map-remove": "remove",
+  "map-keys": "keys",
+  "map-values": "values",
+  "map-has-key": "has-key",
+  "str-length": "length",
+  "str-insert": "insert",
+  "str-index": "index",
+  "str-slice": "slice",
+  unitless: "is-unitless",
+  comparable: "compatible",
+  "list-separator": "separator",
+  "selector-nest": "nest",
+  "selector-append": "append",
+  "selector-replace": "replace",
+  "selector-unify": "unify",
+  "selector-parse": "parse",
+  "selector-extend": "extend"
+};
+
+import { utils } from "stylelint";
+import { atRuleBaseName, namespace } from "../../utils";
+
+export const ruleName = namespace("no-duplicate-mixins");
+
+export const messages = utils.ruleMessages(ruleName, {
+  rejected: mixin => `Unexpected duplicate mixin ${mixin}`
+});
+
+export default function(value) {
+  return (root, result) => {
+    const validOptions = utils.validateOptions(result, ruleName, {
+      actual: value
+    });
+
+    if (!validOptions) {
+      return;
+    }
+
+    const mixins = {};
+
+    root.walkAtRules(decl => {
+      const isMixin = decl.name === "mixin";
+
+      if (!isMixin) {
+        return;
+      }
+
+      const mixinName = atRuleBaseName(decl);
+
+      if (mixins[mixinName]) {
+        utils.report({
+          message: messages.rejected(mixinName),
+          node: decl,
+          result,
+          ruleName
+        });
+      }
+
+      mixins[mixinName] = true;
+    });
+  };
+}
