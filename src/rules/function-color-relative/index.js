efe5bc843ca8be3b26a0bc2e20b40bcaeea8dc5a
krister <krister@sent.at>
2020-04-17 11:12:22 +0300

    function-color-relative: handle filter property

--- src/rules/function-color-relative/index.js
+++ src/rules/function-color-relative/index.js
@@ -19,6 +19,10 @@ const function_names = [
   "fade-out"
 ];
 
+function isColorFunction(node) {
+  return node.type === "function" && function_names.includes(node.value);
+}
+
 function rule(primary) {
   return (root, result) => {
     const validOptions = utils.validateOptions(result, ruleName, {
@@ -36,13 +40,26 @@ function rule(primary) {
           return;
         }
 
-        if (function_names.includes(node.value)) {
-          utils.report({
-            message: messages.rejected,
-            node: decl,
-            index: declarationValueIndex(decl) + node.sourceIndex,
-            result,
-            ruleName
+        const isFilter = decl.prop === "filter";
+        const isSassColorFunction = !isFilter && isColorFunction(node);
+        const isDSFilterColorFunction =
+          isFilter &&
+          node.value === "drop-shadow" &&
+          node.nodes.some(isColorFunction);
+
+        if (isSassColorFunction || isDSFilterColorFunction) {
+          const nodes = isDSFilterColorFunction
+            ? node.nodes.filter(isColorFunction)
+            : [node];
+
+          nodes.forEach(node => {
+            utils.report({
+              message: messages.rejected,
+              node: decl,
+              index: declarationValueIndex(decl) + node.sourceIndex,
+              result,
+              ruleName
+            });
           });
         }
       });
