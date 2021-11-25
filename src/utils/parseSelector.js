d84321680441685ad97681d892442d8a56fedad3
XBOY <xboy2008@live.cn>
2021-11-25 10:57:56 +0800

    fix: use processSync

--- src/utils/parseSelector.js
+++ src/utils/parseSelector.js
@@ -2,7 +2,7 @@ import postCssSelectorParser from "postcss-selector-parser";
 
 export default function(selector, result, node, cb) {
   try {
-    postCssSelectorParser(cb).process(selector);
+    postCssSelectorParser(cb).processSync(selector);
   } catch (e) {
     result.warn("Cannot parse selector", { node });
   }
