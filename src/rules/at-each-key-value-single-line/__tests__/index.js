1926493184a3794c292dfe857cb8a17daa21ebe8
Jan Hähne <github@jhae.de>
2021-12-09 18:57:57 +0100

    Check if custom namespace is used for map* functions in at-each-key-value-single-line rule

--- src/rules/at-each-key-value-single-line/__tests__/index.js
+++ src/rules/at-each-key-value-single-line/__tests__/index.js
@@ -28,7 +28,16 @@ testRule({
         @each $key in map.keys($font-weights) {}
       `,
       description:
-        "Loop that just gets keys + has no need for values when using sass module with default namespace"
+        "Loop that just gets keys + has no need for values when loading sass module with default namespace"
+    },
+    {
+      code: `
+        @use "sass:map";
+        $font-weights: ("regular": 400, "medium": 500, "bold": 700);
+        @each $key in map-keys($font-weights) {}
+      `,
+      description:
+        "Loop that just gets keys + has no need for values when loading sass module with default namespace but using global function"
     },
     {
       code: `
@@ -37,7 +46,16 @@ testRule({
         @each $key in keys($font-weights) {}
       `,
       description:
-        "Loop that just gets keys + has no need for values when using sass module with no namespace"
+        "Loop that just gets keys + has no need for values when loading sass module with no namespace"
+    },
+    {
+      code: `
+        @use "sass:map" as *;
+        $font-weights: ("regular": 400, "medium": 500, "bold": 700);
+        @each $key in map-keys($font-weights) {}
+      `,
+      description:
+        "Loop that just gets keys + has no need for values when loading sass module with no namespace but using global function"
     },
     {
       code: `
@@ -46,7 +64,16 @@ testRule({
         @each $key in ns.keys($font-weights) {}
       `,
       description:
-        "Loop that just gets keys + has no need for values when using sass module with custom namespace"
+        "Loop that just gets keys + has no need for values when loading sass module with custom namespace"
+    },
+    {
+      code: `
+        @use "sass:map" as ns;
+        $font-weights: ("regular": 400, "medium": 500, "bold": 700);
+        @each $key in map-keys($font-weights) {}
+      `,
+      description:
+        "Loop that just gets keys + has no need for values when loading sass module with custom namespace but using global function"
     },
     {
       code: `
@@ -69,7 +96,19 @@ testRule({
         }
       `,
       description:
-        "map.get pattern used with different hash than loop when using sass module with default namespace"
+        "map.get pattern used with different hash than loop when loading sass module with default namespace"
+    },
+    {
+      code: `
+        @use "sass:map";
+        $font-weights: ("regular": 400, "medium": 500, "bold": 700);
+        $other-weights: ("regular": 400, "medium": 500, "bold": 700);
+        @each $key, $value in map-keys($font-weights) {
+         $value: map-get($other-weights, $key);
+        }
+      `,
+      description:
+        "map.get pattern used with different hash than loop when loading sass module with default namespace but using global function"
     },
     {
       code: `
@@ -81,7 +120,19 @@ testRule({
         }
       `,
       description:
-        "map.get pattern used with different hash than loop when using sass module with no namespace"
+        "map.get pattern used with different hash than loop when loading sass module with no namespace"
+    },
+    {
+      code: `
+        @use "sass:map" as *;
+        $font-weights: ("regular": 400, "medium": 500, "bold": 700);
+        $other-weights: ("regular": 400, "medium": 500, "bold": 700);
+        @each $key, $value in map-keys($font-weights) {
+         $value: map-get($other-weights, $key);
+        }
+      `,
+      description:
+        "map.get pattern used with different hash than loop when loading sass module with no namespace but using global function"
     },
     {
       code: `
@@ -93,7 +144,19 @@ testRule({
         }
       `,
       description:
-        "map.get pattern used with different hash than loop when using sass module with custom namespace"
+        "map.get pattern used with different hash than loop when loading sass module with custom namespace"
+    },
+    {
+      code: `
+        @use "sass:map" as ns;
+        $font-weights: ("regular": 400, "medium": 500, "bold": 700);
+        $other-weights: ("regular": 400, "medium": 500, "bold": 700);
+        @each $key, $value in map-keys($font-weights) {
+         $value: map-get($other-weights, $key);
+        }
+      `,
+      description:
+        "map.get pattern used with different hash than loop when loading sass module with custom namespace but using global function"
     }
   ],
 
@@ -119,7 +182,20 @@ testRule({
         }
       `,
       description:
-        "Loop that gets keys + then grabs values inside the map when using sass module with default namespace",
+        "Loop that gets keys + then grabs values inside the map when loading sass module with default namespace",
+      message: messages.expected,
+      line: 4
+    },
+    {
+      code: `
+        @use "sass:map";
+        $font-weights: ("regular": 400, "medium": 500, "bold": 700);
+        @each $key in map-keys($font-weights) {
+          $value: map-get($font-weights, $key);
+        }
+      `,
+      description:
+        "Loop that gets keys + then grabs values inside the map when loading sass module with default namespace but using global function",
       message: messages.expected,
       line: 4
     },
@@ -132,7 +208,20 @@ testRule({
         }
       `,
       description:
-        "Loop that gets keys + then grabs values inside the map when using sass module with no namespace",
+        "Loop that gets keys + then grabs values inside the map when loading sass module with no namespace",
+      message: messages.expected,
+      line: 4
+    },
+    {
+      code: `
+        @use "sass:map" as *;
+        $font-weights: ("regular": 400, "medium": 500, "bold": 700);
+        @each $key in map-keys($font-weights) {
+          $value: map-get($font-weights, $key);
+        }
+      `,
+      description:
+        "Loop that gets keys + then grabs values inside the map when loading sass module with no namespace but using global function",
       message: messages.expected,
       line: 4
     },
@@ -145,7 +234,20 @@ testRule({
         }
       `,
       description:
-        "Loop that gets keys + then grabs values inside the map when using sass module with custom namespace",
+        "Loop that gets keys + then grabs values inside the map when loading sass module with custom namespace",
+      message: messages.expected,
+      line: 4
+    },
+    {
+      code: `
+        @use "sass:map" as ns;
+        $font-weights: ("regular": 400, "medium": 500, "bold": 700);
+        @each $key in map-keys($font-weights) {
+          $value: map-get($font-weights, $key);
+        }
+      `,
+      description:
+        "Loop that gets keys + then grabs values inside the map when loading sass module with custom namespace but using global function",
       message: messages.expected,
       line: 4
     }
--- src/rules/at-each-key-value-single-line/index.js
+++ src/rules/at-each-key-value-single-line/index.js
@@ -1,5 +1,5 @@
 import { utils } from "stylelint";
-import { namespace, moduleNamespace } from "../../utils";
+import { moduleNamespace, namespace } from "../../utils";
 
 export const ruleName = namespace("at-each-key-value-single-line");
 
@@ -77,13 +77,17 @@ function separateEachParams(paramString) {
 }
 
 function didCallMapKeys(mapDecl, mapNamespace) {
-  const pattern = getNamespacedPattern("keys\\(.*\\)", mapNamespace);
+  const pattern = getNamespacedPattern("keys\\(.*\\)", mapNamespace, mapDecl);
 
   return new RegExp(pattern).test(mapDecl);
 }
 
 function didCallMapGet(mapDecl, mapNamespace) {
-  const pattern = getNamespacedPattern("get\\((.*),(.*)\\)", mapNamespace);
+  const pattern = getNamespacedPattern(
+    "get\\((.*),(.*)\\)",
+    mapNamespace,
+    mapDecl
+  );
 
   return new RegExp(pattern).test(mapDecl);
 }
@@ -91,7 +95,11 @@ function didCallMapGet(mapDecl, mapNamespace) {
 // Fetch the name of the map from a map-keys() or map.keys() call.
 function mapName(mapDecl, mapNamespace) {
   if (didCallMapKeys(mapDecl, mapNamespace)) {
-    const pattern = getNamespacedPattern("keys\\((.*)\\)", mapNamespace);
+    const pattern = getNamespacedPattern(
+      "keys\\((.*)\\)",
+      mapNamespace,
+      mapDecl
+    );
 
     return mapDecl.match(new RegExp(pattern))[1];
   }
@@ -102,19 +110,26 @@ function mapName(mapDecl, mapNamespace) {
 // Returns the parameters of a map-get or map.get call
 // Returns [map variable, key_variable]
 function mapGetParameters(mapGetDecl, mapNamespace) {
-  const pattern = getNamespacedPattern("get\\((.*), ?(.*)\\)", mapNamespace);
+  const pattern = getNamespacedPattern(
+    "get\\((.*), ?(.*)\\)",
+    mapNamespace,
+    mapGetDecl
+  );
   const parts = mapGetDecl.match(new RegExp(pattern));
 
   return [parts[1], parts[2]];
 }
 
-function getNamespacedPattern(pattern, mapNamespace) {
-  switch (mapNamespace) {
-    case "*":
-      return pattern;
-    case null:
-      return `map[-.]${pattern}`;
+function getNamespacedPattern(pattern, namespace, decl) {
+  if (namespace === null) {
+    return `map[-.]${pattern}`;
   }
 
-  return `${mapNamespace}[-.]${pattern}`;
+  if (namespace === "*") {
+    return pattern;
+  }
+
+  return new RegExp(`${namespace}.${pattern}`).test(decl)
+    ? `${namespace}.${pattern}`
+    : `map-${pattern}`;
 }
