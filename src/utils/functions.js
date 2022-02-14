e3ae40cdce89ff5822adbf7da766b6854404af9a
Masafumi Koba <473530+ybiquitous@users.noreply.github.com>
2022-02-15 01:11:06 +0900

    Update functions list

--- src/utils/functions.js
+++ src/utils/functions.js
@@ -1,7 +1,13 @@
 /**
  * @see https://sass-lang.com/documentation/modules#global-functions
  */
-export const GLOBAL_FUNCTIONS = Object.freeze(["if"]);
+export const GLOBAL_FUNCTIONS = Object.freeze([
+  "hsl",
+  "hsla",
+  "if",
+  "rgb",
+  "rgba"
+]);
 
 /**
  * @see https://sass-lang.com/documentation/modules/color
@@ -19,8 +25,8 @@ export const COLOR_FUNCTIONS = Object.freeze([
   "fade-in",
   "fade-out",
   "grayscale",
+  "green",
   "hue",
-  "hwb",
   "ie-hex-str",
   "invert",
   "lighten",
@@ -31,12 +37,84 @@ export const COLOR_FUNCTIONS = Object.freeze([
   "saturate",
   "saturation",
   "scale-color",
-  "transparentize"
+  "transparentize",
+
+  // with namespace
+  "color.adjust",
+  "color.alpha",
+  "color.blackness",
+  "color.blue",
+  "color.change",
+  "color.complement",
+  "color.grayscale",
+  "color.green",
+  "color.hue",
+  "color.hwb",
+  "color.ie-hex-str",
+  "color.invert",
+  "color.lightness",
+  "color.mix",
+  "color.red",
+  "color.saturation",
+  "color.scale",
+  "color.whiteness"
+]);
+
+/**
+ * @see https://sass-lang.com/documentation/modules/list
+ */
+export const LIST_FUNCTIONS = Object.freeze([
+  "append",
+  "index",
+  "is-bracketed",
+  "join",
+  "length",
+  "list-separator",
+  "nth",
+  "set-nth",
+  "list.zip",
+
+  // with namespace
+  "list.append",
+  "list.index",
+  "list.is-bracketed",
+  "list.join",
+  "list.length",
+  "list.nth",
+  "list.separator",
+  "list.set-nth",
+  "list.slash",
+  "zip"
+]);
+
+/**
+ * @see https://sass-lang.com/documentation/modules/map
+ */
+export const MAP_FUNCTIONS = Object.freeze([
+  "map-get",
+  "map-has-key",
+  "map-keys",
+  "map-merge",
+  "map-remove",
+  "map-values",
+
+  // with namespace
+  "map.deep-merge",
+  "map.deep-remove",
+  "map.get",
+  "map.has-key",
+  "map.keys",
+  "map.merge",
+  "map.remove",
+  "map.set",
+  "map.values"
 ]);
 
 // TODO: Add other functions...
 
 export const ALL_FUNCTIONS = Object.freeze([
   ...GLOBAL_FUNCTIONS,
-  ...COLOR_FUNCTIONS
+  ...COLOR_FUNCTIONS,
+  ...LIST_FUNCTIONS,
+  ...MAP_FUNCTIONS
 ]);
