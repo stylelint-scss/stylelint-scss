9f32e587af8e88612218a090b4b30af206185605
Masafumi Koba <473530+ybiquitous@users.noreply.github.com>
2022-08-23 09:28:51 +0900

    Fix `at-rule-no-unknown` end positions
    This change also adds a test helper function to make testing multiline strings easier.

--- jest-setup.js
+++ jest-setup.js
@@ -1,3 +1,10 @@
 const getTestRule = require("jest-preset-stylelint/getTestRule");
 
 global.testRule = getTestRule({ plugins: ["./src"] });
+
+// Inspired by https://www.npmjs.com/package/dedent-tag
+global.dedent = function dedent(strings) {
+  return strings[0]
+    .replace(/^[ \t]+/gm, "") // remove indentation
+    .replace(/^\s*\n/gm, ""); // remove empty lines
+};
