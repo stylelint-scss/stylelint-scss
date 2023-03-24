632911b7524ea29fb70f45a7e5bdcdde3f94f2a7
XhmikosR <xhmikosr@gmail.com>
2023-03-24 15:28:05 +0200

    Move things outside of package.json

---
+++ jest.config.js
@@ -0,0 +1,20 @@
+module.exports = {
+  preset: "jest-preset-stylelint",
+  clearMocks: true,
+  collectCoverage: false,
+  collectCoverageFrom: ["src/**/*.js"],
+  coverageDirectory: "./.coverage/",
+  coverageReporters: ["lcov", "text"],
+  coverageThreshold: {
+    global: {
+      branches: 75,
+      functions: 75,
+      lines: 75,
+      statements: 75
+    }
+  },
+  setupFiles: ["./jest-setup.js"],
+  testEnvironment: "node",
+  roots: ["src"],
+  testRegex: ".*\\.test\\.js$|src/.*/__tests__/.*\\.js$"
+};
