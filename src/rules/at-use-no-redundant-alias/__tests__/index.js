0d6ddd80ac3bac08adebf53eeda8190e33fd03dc
Pamela Lozano <pamelalozano16@gmail.com>
2023-10-02 16:31:46 -0700

    at-use-no-redundant-alias rule fix

--- src/rules/at-use-no-redundant-alias/__tests__/index.js
+++ src/rules/at-use-no-redundant-alias/__tests__/index.js
@@ -93,3 +93,78 @@ testRule({
     }
   ]
 });
+
+// Test with fix
+testRule({
+  ruleName,
+  config: [true],
+  customSyntax: "postcss-scss",
+  fix: true,
+
+  accept: [
+    {
+      code: `
+      @use "foo";
+      `,
+      description: "Default namespace"
+    }
+  ],
+  reject: [
+    {
+      code: `@use "foo" as foo;`,
+      fixed: `@use "foo";`,
+      line: 1,
+      column: 1,
+      message: messages.rejected,
+      description: "Without url"
+    },
+    {
+      code: `@use "src/corners" as corners;`,
+      fixed: `@use "src/corners";`,
+      line: 1,
+      column: 1,
+      message: messages.rejected,
+      description: "With url"
+    },
+    {
+      code: `@use "sass:math" as math;`,
+      fixed: `@use "sass:math";`,
+      line: 1,
+      column: 1,
+      message: messages.rejected,
+      description: "Built-in module"
+    },
+    {
+      code: `@use "foo"   as   foo;`,
+      fixed: `@use "foo";`,
+      line: 1,
+      column: 1,
+      message: messages.rejected,
+      description: "Without space"
+    },
+    {
+      code: `@use "foo" as foo with ($baz: 1px);`,
+      fixed: `@use "foo" with ($baz: 1px);`,
+      line: 1,
+      column: 1,
+      message: messages.rejected,
+      description: "Configured"
+    },
+    {
+      code: `
+      @use "sass:color" as color with (
+        $baz: 1px
+      );
+    `,
+      fixed: `
+      @use "sass:color" with (
+        $baz: 1px
+      );
+    `,
+      line: 2,
+      column: 7,
+      message: messages.rejected,
+      description: "Configured multiline"
+    }
+  ]
+});
