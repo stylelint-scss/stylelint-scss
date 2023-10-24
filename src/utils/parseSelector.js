2a595b38db3de0588a272594ff66cebd453ba3ce
William Lewis <91909230+wlewis-formative@users.noreply.github.com>
2023-10-24 14:11:40 -0400

    Add styleLintType: "parseError" when parseSelector fails (#858)

--- src/utils/parseSelector.js
+++ src/utils/parseSelector.js
@@ -6,6 +6,6 @@ module.exports = function (selector, result, node, cb) {
   try {
     return postCssSelectorParser(cb).processSync(selector);
   } catch (e) {
-    result.warn("Cannot parse selector", { node });
+    result.warn("Cannot parse selector", { node, stylelintType: "parseError" });
   }
 };
