e4e8a02f20dd05a132c74a7fc2977a8a914983f4
Pamela Lozano <30474787+pamelalozano16@users.noreply.github.com>
2024-02-22 17:26:04 -0800

    Fix variables used as parameters and skip files using @import (#957)

--- src/rules/no-unused-private-members/__tests__/index.js
+++ src/rules/no-unused-private-members/__tests__/index.js
@@ -38,6 +38,18 @@ testRule({
     },
     {
       code: `
+      @mixin _reset-list($n1) {
+        margin: $n1;
+      }
+      
+      nav ul {
+        @include _reset-list(2px);
+      }      
+      `,
+      description: "Private mixin"
+    },
+    {
+      code: `
       $-radius: 3px;
 
       .rounded {
@@ -66,17 +78,47 @@ testRule({
     },
     {
       code: `
-      @import "fff"
-      $-radius: 3px;
+      @import 'foo';
 
-      rounded {
-        border-radius: 0px;
+      %-toolbelt:hover {
+        color: red;
+      }
+
+      .a.%-b {
+        margin: 1px;
+      }
+
+      .action-buttons {
+        @extend %-toolbelt;
+        color: blue;
+      }
+      `
+    },
+    {
+      code: `
+      $-radius: 3px;
+      
+      .action-buttons {
+        margin: map.get($-radius);
       }
       `,
-      message: messages.expected("$-radius"),
-      description: "Skip files using @import",
-      line: 2,
-      column: 7
+      description: "map.get uses variable"
+    },
+    {
+      code: `
+      $_sys-color: #0000;
+      $_a-color: #fff;
+      $_b-color: map.get($_sys-color);
+
+      .btn {
+          @include button_text_theme.theme (
+            (
+            'label-text-color': $_b-color,
+            'label-text-color': $_a-color
+            )
+          );
+      }`,
+      description: "Is in theme declaration"
     }
   ],
 
@@ -167,6 +209,55 @@ testRule({
       line: 6,
       column: 7,
       description: "Private placeholder selector with class"
+    },
+    {
+      code: `
+      $_a: 5px * 2;
+      $_b: 900;
+      $_c: 400;
+
+      .chip {
+        @include header($_b);
+        @include column($_a);
+        @include footer();
+      }`,
+      message: messages.expected("$_c"),
+      line: 4,
+      column: 7,
+      description: "Variables used as function parameters"
+    },
+    {
+      code: `
+      $_a: 5px * 2;
+      $_b: 900;
+      
+      .action-buttons {
+        margin: map.get($_a);
+      }
+      `,
+      message: messages.expected("$_b"),
+      line: 3,
+      column: 7,
+      description: "map.get uses variable"
+    },
+    {
+      code: `
+      $_sys-color: #0000;
+      $_a-color: #fff;
+      $_b-color: map.get($_sys-color);
+
+      .btn {
+          @include button_text_theme.theme (
+            (
+            'label-text-color': $_b-color,
+            'label-text-color': #000
+            )
+          );
+      }`,
+      message: messages.expected("$_a-color"),
+      line: 3,
+      column: 7,
+      description: "Is in theme declaration"
     }
   ]
 });
