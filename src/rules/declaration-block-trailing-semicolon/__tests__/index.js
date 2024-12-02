97317b6029813dc82c51ae6ae0ce57284bdef6f7
Pamela Lozano <pamelalozano16@gmail.com>
2024-12-02 14:52:23 -0800

    Mirror declaration-block-trailing-semicolon from stylelint

---
+++ src/rules/declaration-block-trailing-semicolon/__tests__/index.js
@@ -0,0 +1,212 @@
+"use strict";
+
+const { ruleName, messages } = require("..");
+
+testRule({
+  ruleName,
+  customSyntax: "postcss-scss",
+  config: [`always`],
+
+  accept: [
+    {
+      code: `a { color: pink; }`,
+      description: `single declaration block with trailing semicolon`
+    },
+    {
+      code: `a { background: orange; color: pink; }`,
+      description: `multi declaration block with trailing semicolon`
+    },
+    {
+      code: `a { &:hover { color: pink; }}`,
+      description: `nesting without first-level decl`
+    },
+    {
+      code: `a { color: red; &:hover { color: pink; }}`,
+      description: `nesting with first-level decl`
+    }
+  ],
+
+  reject: [
+    {
+      code: `a { color: pink }`,
+      description: `single declaration block without trailing semicolon`,
+      message: messages.expected,
+      line: 1,
+      column: 15
+    },
+    {
+      code: `a { background: orange; color: pink }`,
+      description: `multi declaration block without trailing semicolon`,
+      message: messages.expected,
+      line: 1,
+      column: 35
+    },
+    {
+      code: `a { &:hover { color: pink }}`,
+      description: `nesting without first-level decl`,
+      message: messages.expected,
+      line: 1,
+      column: 25
+    },
+    {
+      code: `a { color: red; &:hover { color: pink }}`,
+      description: `nesting with first-level decl`,
+      message: messages.expected,
+      line: 1,
+      column: 37
+    }
+  ]
+});
+
+testRule({
+  ruleName,
+  customSyntax: "postcss-scss",
+  config: [`always`, { ignore: `single-declaration` }],
+
+  accept: [
+    {
+      code: `a { color: pink }`,
+      description: `single declaration without trailing semicolon`
+    },
+    {
+      code: `a { color: pink; }`,
+      description: `single declaration with trailing semicolon`
+    },
+    {
+      code: `@keyframes foo { from { top: 0px } to { top: 1px; } }`,
+      description: `inconsistent case (with and without)`
+    }
+  ],
+
+  reject: [
+    {
+      code: `a { background: orange; color: pink }`,
+      description: `multi declaration block without trailing semicolon`,
+      message: messages.expected,
+      line: 1,
+      column: 35
+    }
+  ]
+});
+
+testRule({
+  ruleName,
+  customSyntax: "postcss-scss",
+  config: [`never`, { ignore: [`single-declaration`] }],
+
+  accept: [
+    {
+      code: `a { color: pink }`,
+      description: `single declaration without trailing semicolon`
+    },
+    {
+      code: `a { color: pink; }`,
+      description: `single declaration with trailing semicolon`
+    },
+    {
+      code: `@keyframes foo { from { top: 0px } to { top: 1px; } }`,
+      description: `inconsistent case (with and without)`
+    }
+  ]
+});
+
+testRule({
+  ruleName,
+  config: [`never`],
+
+  accept: [
+    {
+      code: `a { color: pink }`,
+      description: `single-line declaration block without trailing semicolon`
+    },
+    {
+      code: `a { background: orange; color: pink }`,
+      description: `multi-line declaration block without trailing semicolon`
+    }
+  ],
+
+  reject: [
+    {
+      code: `a { color: pink; }`,
+      description: `single-line declaration block with trailing semicolon`,
+      message: messages.rejected,
+      line: 1,
+      column: 15
+    },
+    {
+      code: `a { background: orange; color: pink; }`,
+      description: `multi-line declaration block with trailing semicolon`,
+      message: messages.rejected,
+      line: 1,
+      column: 35
+    }
+  ]
+});
+
+testRule({
+  ruleName,
+  config: [`always`],
+  customSyntax: `postcss-scss`,
+
+  accept: [
+    {
+      code: `a { @includes foo; }`,
+      description: `at-rule with trailing semicolon`
+    },
+    {
+      code: `a { @foo { color: pink; } }`,
+      description: `at-rule with decl block with trailing semicolon`
+    }
+  ],
+
+  reject: [
+    {
+      code: `a { @includes foo }`,
+      description: `at-rule without trailing semicolon`,
+      message: messages.expected,
+      line: 1,
+      column: 17
+    },
+    {
+      code: `a { @foo { color: pink } }`,
+      description: `at-rule with decl block without trailing semicolon`,
+      message: messages.expected,
+      line: 1,
+      column: 22
+    }
+  ]
+});
+
+testRule({
+  ruleName,
+  config: [`never`],
+  customSyntax: `postcss-scss`,
+
+  accept: [
+    {
+      code: `a { @includes foo }`,
+      description: `at-rule without trailing semicolon`
+    },
+    {
+      code: `a { @foo { color: pink } }`,
+      description: `at-rule with decl block without trailing semicolon`
+    }
+  ],
+
+  reject: [
+    {
+      code: `a { @includes foo; }`,
+      description: `at-rule with trailing semicolon`,
+      message: messages.rejected,
+      line: 1,
+      column: 17
+    },
+    {
+      code: `a { @foo { color: pink; } }`,
+      description: `at-rule with decl block with trailing semicolon`,
+      message: messages.rejected,
+      line: 1,
+      column: 22
+    }
+  ]
+});
---
+++ src/rules/declaration-block-trailing-semicolon/index.js
@@ -0,0 +1,117 @@
+"use strict";
+
+const { utils } = require("stylelint");
+const hasBlock = require("../../utils/hasBlock.js");
+const { isAtRule } = require("../../utils/typeGuards.js");
+const optionsMatches = require("../../utils/optionsMatches");
+const namespace = require("../../utils/namespace");
+const ruleUrl = require("../../utils/ruleUrl");
+
+const ruleName = namespace("declaration-block-trailing-semicolon");
+
+const messages = utils.ruleMessages(ruleName, {
+  expected: `Expected a trailing semicolon`,
+  rejected: `Unexpected trailing semicolon`
+});
+
+const meta = {
+  url: ruleUrl(ruleName)
+};
+
+function rule(primary, secondaryOptions, context) {
+  return (root, result) => {
+    const validOptions = utils.validateOptions(
+      result,
+      ruleName,
+      {
+        actual: primary,
+        possible: [`always`, `never`]
+      },
+      {
+        actual: secondaryOptions,
+        possible: {
+          ignore: [`single-declaration`]
+        },
+        optional: true
+      }
+    );
+
+    if (!validOptions) {
+      return;
+    }
+
+    root.walkAtRules(atRule => {
+      if (!atRule.parent) throw new Error(`A parent node must be present`);
+      if (
+        atRule.parent === root ||
+        atRule !== atRule.parent.last ||
+        hasBlock(atRule)
+      )
+        return;
+      checkLastNode(atRule);
+    });
+
+    root.walkDecls(decl => {
+      if (!decl.parent) throw new Error(`A parent node must be present`);
+      if (decl.parent.type === `object` || decl !== decl.parent.last) return;
+      checkLastNode(decl);
+    });
+
+    /**
+     * @param {import('postcss').Node} node
+     */
+    function checkLastNode(node) {
+      if (!node.parent) {
+        throw new Error(`A parent node must be present`);
+      }
+
+      const hasSemicolon = node.parent.raws.semicolon;
+      const ignoreSingleDeclaration = optionsMatches(
+        secondaryOptions,
+        `ignore`,
+        `single-declaration`
+      );
+
+      if (ignoreSingleDeclaration && node.parent.first === node) return;
+      if (primary === `always` && primary === `never`)
+        throw new Error(`Unexpected primary option: "${primary}"`);
+
+      let message;
+
+      if (primary === `always` && !hasSemicolon) {
+        message = messages.expected;
+      } else if (primary === `never` && hasSemicolon) {
+        message = messages.rejected;
+      }
+
+      if (context.fix) {
+        if (primary === `always` && !hasSemicolon) {
+          node.parent.raws.semicolon = true;
+
+          if (isAtRule(node)) {
+            node.raws.between = ``;
+            node.parent.raws.after = ` `;
+          }
+        } else if (primary === `never` && hasSemicolon) {
+          node.parent.raws.semicolon = false;
+        }
+      }
+
+      if (message) {
+        utils.report({
+          message,
+          node,
+          index: node.toString().trim().length - 1,
+          result,
+          ruleName
+        });
+      }
+    }
+  };
+}
+
+rule.ruleName = ruleName;
+rule.messages = messages;
+rule.meta = meta;
+
+module.exports = rule;
