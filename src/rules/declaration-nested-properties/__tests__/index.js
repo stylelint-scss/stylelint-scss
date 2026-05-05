0d74b4543b01037b601354007cdbb87c34ab5ee7
Jan Hähne <github@jhae.de>
2026-05-05 20:52:07 +0200

    Add support for `postcss-scss` in `declaration-nested-properties` rule (#1265)
    * Fix handling of nested declarations in declaration-nested-properties rule
    
    * Refactor declaration-nested-properties rule for performance and clarity

--- src/rules/declaration-nested-properties/__tests__/index.js
+++ src/rules/declaration-nested-properties/__tests__/index.js
@@ -2,37 +2,42 @@ import rule from "../index.js";
 
 const { ruleName, messages } = rule;
 
+const syntaxVariants = [
+  { syntaxLabel: "default syntax" },
+  { syntaxLabel: "postcss-scss", customSyntax: "postcss-scss" }
+];
+
+const withSyntaxLabel = (syntaxLabel, testCases) =>
+  testCases.map(testCase => ({
+    ...testCase,
+    description: `${testCase.description} (${syntaxLabel})`
+  }));
+
 // --------------------------------------------------------------------------
 // always
 // --------------------------------------------------------------------------
 
-testRule({
-  ruleName,
-  config: ["always"],
-  customSyntax: "postcss-scss",
-  skipBasicChecks: true,
-
-  accept: [
-    {
-      code: `
+const alwaysAcceptCases = [
+  {
+    code: `
       .funky {
         background: no-repeat;
         margin: 1px;
       }
     `,
-      description: "{ always } Nothing to nest"
-    },
-    {
-      code: `
+    description: "{ always } Nothing to nest"
+  },
+  {
+    code: `
       .funky {
         -webkit-box-sizing: border-box;
         -webkit-box-shadow: 1px 0 0 red;
       }
     `,
-      description: "{ always } Vendor prefixed rules."
-    },
-    {
-      code: `
+    description: "{ always } Vendor prefixed rules."
+  },
+  {
+    code: `
       a {
         font: {
           size: 10px;
@@ -40,73 +45,72 @@ testRule({
         }
       }
       `,
-      description: "{ always } nested properties"
-    },
-    {
-      code: `
+    description: "{ always } nested properties"
+  },
+  {
+    code: `
       a {
         background: {
           color: red
         };
       }
     `,
-      description: "{ always } one `background` in nested form"
-    }
-  ],
+    description: "{ always } one `background` in nested form"
+  }
+];
 
-  reject: [
-    {
-      code: `
+const alwaysRejectCases = [
+  {
+    code: `
       a {
         background-color: red;
         background-repeat: no-repeat;
       }
     `,
-      description:
-        "{ always } Simple test: background-color, background-repeat",
-      warnings: [
-        {
-          line: 3,
-          column: 9,
-          message: messages.expected("background-color")
-        },
-        {
-          line: 4,
-          column: 9,
-          message: messages.expected("background-repeat")
-        }
-      ]
-    },
-    {
-      code: `
+    description: "{ always } Simple test: background-color, background-repeat",
+    warnings: [
+      {
+        line: 3,
+        column: 9,
+        message: messages.expected("background-color")
+      },
+      {
+        line: 4,
+        column: 9,
+        message: messages.expected("background-repeat")
+      }
+    ]
+  },
+  {
+    code: `
       a {
         background-color: red;
         @media (color) { background-image: url(img.png); }
         background-repeat: no-repeat;
       }
     `,
-      description:
-        "{ always } background-color, background-repeat separated by at-rule",
-      warnings: [
-        {
-          line: 3,
-          column: 9,
-          message: messages.expected("background-color")
-        },
-        {
-          line: 5,
-          column: 9,
-          message: messages.expected("background-repeat")
-        },
-        {
-          line: 4,
-          column: 26,
-          message: messages.expected("background-image")
-        }
-      ]
-    },
-    {
-      code: `
+    description:
+      "{ always } background-color, background-repeat separated by at-rule",
+    warnings: [
+      {
+        line: 3,
+        column: 9,
+        message: messages.expected("background-color")
+      },
+      {
+        line: 5,
+        column: 9,
+        message: messages.expected("background-repeat")
+      },
+      {
+        line: 4,
+        column: 26,
+        message: messages.expected("background-image")
+      }
+    ]
+  },
+  {
+    code: `
       a {
         background: url(img.png) no-repeat {
           color: red
@@ -114,17 +118,17 @@ testRule({
         background-position: center;
       }
     `,
-      description: "{ always } nested `background` and background-position",
-      warnings: [
-        {
-          line: 6,
-          column: 9,
-          message: messages.expected("background-position")
-        }
-      ]
-    },
-    {
-      code: `
+    description: "{ always } nested `background` and background-position",
+    warnings: [
+      {
+        line: 6,
+        column: 9,
+        message: messages.expected("background-position")
+      }
+    ]
+  },
+  {
+    code: `
       a {
         background:   url(img.png) no-repeat {
           color: red
@@ -132,17 +136,17 @@ testRule({
         background-position: center;
       }
     `,
-      description: "{ always } `prop:    value {nested} prop-v: value`.",
-      warnings: [
-        {
-          line: 6,
-          column: 9,
-          message: messages.expected("background-position")
-        }
-      ]
-    },
-    {
-      code: `
+    description: "{ always } `prop:    value {nested} prop-v: value`.",
+    warnings: [
+      {
+        line: 6,
+        column: 9,
+        message: messages.expected("background-position")
+      }
+    ]
+  },
+  {
+    code: `
       a {
         background  : url(img.png) no-repeat {
           color: red
@@ -150,40 +154,58 @@ testRule({
         background-position: center;
       }
     `,
-      description: "{ always } `prop  :  value {nested} prop-v: value`.",
-      warnings: [
-        {
-          line: 6,
-          column: 9,
-          message: messages.expected("background-position")
-        }
-      ]
-    }
-  ]
+    description: "{ always } `prop  :  value {nested} prop-v: value`.",
+    warnings: [
+      {
+        line: 6,
+        column: 9,
+        message: messages.expected("background-position")
+      }
+    ]
+  }
+];
+
+// Run test with basic checks
+testRule({
+  ruleName,
+  config: ["always"]
 });
+
+// Run tests for each syntax variant
+for (const { syntaxLabel, ...options } of syntaxVariants) {
+  testRule({
+    ruleName,
+    config: ["always"],
+    ...options,
+    skipBasicChecks: true,
+    accept: withSyntaxLabel(syntaxLabel, alwaysAcceptCases)
+  });
+
+  testRule({
+    ruleName,
+    config: ["always"],
+    ...options,
+    skipBasicChecks: true,
+    reject: withSyntaxLabel(syntaxLabel, alwaysRejectCases)
+  });
+}
 
 // --------------------------------------------------------------------------
 // always, except: only-of-namespace
 // --------------------------------------------------------------------------
 
-testRule({
-  ruleName,
-  config: ["always", { except: "only-of-namespace" }],
-  customSyntax: "postcss-scss",
-  skipBasicChecks: true,
-
-  accept: [
-    {
-      code: `
+const alwaysExceptAcceptCases = [
+  {
+    code: `
       a {
         position: absolute;
         background-color: red;
       }
     `,
-      description: "{ always, except: only-of-namespace } background-color only"
-    },
-    {
-      code: `
+    description: "{ always, except: only-of-namespace } background-color only"
+  },
+  {
+    code: `
       a {
         background:red {
           origin: padding-box;
@@ -191,11 +213,11 @@ testRule({
         background-position: center;
       }
     `,
-      description:
-        "{ always, except: only-of-namespace } `background:red`, one rule inside"
-    },
-    {
-      code: `
+    description:
+      "{ always, except: only-of-namespace } `background:red`, one rule inside"
+  },
+  {
+    code: `
       a {
         background: red {
           origin: padding-box;
@@ -203,37 +225,37 @@ testRule({
         }
       }
     `,
-      description:
-        "{ always, except: only-of-namespace } background, two rules inside"
-    }
-  ],
+    description:
+      "{ always, except: only-of-namespace } background, two rules inside"
+  }
+];
 
-  reject: [
-    {
-      code: `
+const alwaysExceptRejectCases = [
+  {
+    code: `
       a {
         background-color: red;
         @media (color) { background-image: url(img.png); }
         background-repeat: no-repeat;
       }
     `,
-      description:
-        "{ always, except: only-of-namespace } background-color, background-repeat separated by at-rule",
-      warnings: [
-        {
-          line: 3,
-          column: 9,
-          message: messages.expected("background-color")
-        },
-        {
-          line: 5,
-          column: 9,
-          message: messages.expected("background-repeat")
-        }
-      ]
-    },
-    {
-      code: `
+    description:
+      "{ always, except: only-of-namespace } background-color, background-repeat separated by at-rule",
+    warnings: [
+      {
+        line: 3,
+        column: 9,
+        message: messages.expected("background-color")
+      },
+      {
+        line: 5,
+        column: 9,
+        message: messages.expected("background-repeat")
+      }
+    ]
+  },
+  {
+    code: `
       a {
         background: {
           origin: padding-box;
@@ -243,40 +265,40 @@ testRule({
         }
       }
     `,
-      description: "{ always, except: only-of-namespace } `prop:`, one rule X2",
-      warnings: [
-        {
-          line: 3,
-          column: 9,
-          message: messages.rejected("background")
-        },
-        {
-          line: 6,
-          column: 9,
-          message: messages.rejected("background")
-        }
-      ]
-    },
-    {
-      code: `
+    description: "{ always, except: only-of-namespace } `prop:`, one rule X2",
+    warnings: [
+      {
+        line: 3,
+        column: 9,
+        message: messages.rejected("background")
+      },
+      {
+        line: 6,
+        column: 9,
+        message: messages.rejected("background")
+      }
+    ]
+  },
+  {
+    code: `
       a {
         background: red {
           origin: padding-box;
         }
       }
     `,
-      description:
-        "{ always, except: only-of-namespace } `prop: value`, one rule inside",
-      warnings: [
-        {
-          line: 3,
-          column: 9,
-          message: messages.rejected("background")
-        }
-      ]
-    },
-    {
-      code: `
+    description:
+      "{ always, except: only-of-namespace } `prop: value`, one rule inside",
+    warnings: [
+      {
+        line: 3,
+        column: 9,
+        message: messages.rejected("background")
+      }
+    ]
+  },
+  {
+    code: `
       a {
         background: red {
           origin: padding-box;
@@ -285,57 +307,68 @@ testRule({
         background-position: center;
       }
     `,
-      description:
-        "{ always, except: only-of-namespace } `background:red`, one rule inside 2",
-      warnings: [
-        {
-          line: 7,
-          column: 9,
-          message: messages.expected("background-position")
-        }
-      ]
-    }
-  ]
-});
+    description:
+      "{ always, except: only-of-namespace } `background:red`, one rule inside 2",
+    warnings: [
+      {
+        line: 7,
+        column: 9,
+        message: messages.expected("background-position")
+      }
+    ]
+  }
+];
+
+for (const { syntaxLabel, ...options } of syntaxVariants) {
+  testRule({
+    ruleName,
+    config: ["always", { except: "only-of-namespace" }],
+    ...options,
+    skipBasicChecks: true,
+    accept: withSyntaxLabel(syntaxLabel, alwaysExceptAcceptCases)
+  });
+
+  testRule({
+    ruleName,
+    config: ["always", { except: "only-of-namespace" }],
+    ...options,
+    skipBasicChecks: true,
+    reject: withSyntaxLabel(syntaxLabel, alwaysExceptRejectCases)
+  });
+}
 
 // --------------------------------------------------------------------------
 // never
 // --------------------------------------------------------------------------
 
-testRule({
-  ruleName,
-  config: ["never"],
-  customSyntax: "postcss-scss",
-  skipBasicChecks: true,
-
-  accept: [
-    {
-      code: `
+const neverAcceptCases = [
+  {
+    code: `
       a {
         background: red;
       }
     `,
-      description: "{ never } bg shorthand."
-    },
-    {
-      code: `
+    description: "{ never } bg shorthand."
+  },
+  {
+    code: `
       a {
         background-color: red;
       }
     `,
-      description: "{ never } bgc."
-    },
-    {
-      code: `
+    description: "{ never } bgc."
+  },
+  {
+    code: `
       a {
         background-color: red;
         background-repeat: no-repeat;
       }
     `,
-      description: "{ never } bgc, bgr."
-    },
-    {
-      code: `
+    description: "{ never } bgc, bgr."
+  },
+  {
+    code: `
       a {
         background:red {
           repeat: no-repeat;
@@ -343,49 +376,49 @@ testRule({
         }
       }
     `,
-      description: "{ never } `prop:value` -- selector."
-    },
-    {
-      code: `.class {
+    description: "{ never } `prop:value` -- selector."
+  },
+  {
+    code: `.class {
       &:not(.other-class) { }
     }`,
-      description: "{ never } `.class { &:not() { ... } }` -- selector."
-    },
-    {
-      code: ".test4\\:3 {}",
-      description: "{ never } selector with escaping"
-    }
-  ],
+    description: "{ never } `.class { &:not() { ... } }` -- selector."
+  },
+  {
+    code: ".test4\\:3 {}",
+    description: "{ never } selector with escaping"
+  }
+];
 
-  reject: [
-    /* {
-      code: `
+const neverRejectCases = [
+  {
+    code: `
       a {
         background: red {
           repeat: no-repeat;
         }
       }
     `,
-      description: "{ never } `prop: { one nested }`.",
-      message: messages.rejected("background"),
-      line: 3,
-      column: 9
-    }, */
-    {
-      code: `
+    description: "{ never } `prop: value { one nested }`.",
+    message: messages.rejected("background"),
+    line: 3,
+    column: 9
+  },
+  {
+    code: `
       a {
         background: {
           repeat: no-repeat;
         }
       }
     `,
-      description: "{ never } `prop: value { one nested }`.",
-      message: messages.rejected("background"),
-      line: 3,
-      column: 9
-    },
-    {
-      code: `
+    description: "{ never } `prop: value { one nested }`.",
+    message: messages.rejected("background"),
+    line: 3,
+    column: 9
+  },
+  {
+    code: `
       a {
         b {
           @media (color) {
@@ -397,13 +430,13 @@ testRule({
         }
       }
     `,
-      description: "{ never } `prop: { two nested }`, deep inside a rule.",
-      message: messages.rejected("background"),
-      line: 5,
-      column: 13
-    },
-    {
-      code: `
+    description: "{ never } `prop: { two nested }`, deep inside a rule.",
+    message: messages.rejected("background"),
+    line: 5,
+    column: 13
+  },
+  {
+    code: `
       a {
         -webkit: {
           box-sizing: border-box;
@@ -411,10 +444,27 @@ testRule({
         }
       }
     `,
-      description: "{ never } `-webkit: { ... }`.",
-      message: messages.rejected("-webkit"),
-      line: 3,
-      column: 9
-    }
-  ]
-});
+    description: "{ never } `-webkit: { ... }`.",
+    message: messages.rejected("-webkit"),
+    line: 3,
+    column: 9
+  }
+];
+
+for (const { syntaxLabel, ...options } of syntaxVariants) {
+  testRule({
+    ruleName,
+    config: ["never"],
+    ...options,
+    skipBasicChecks: true,
+    accept: withSyntaxLabel(syntaxLabel, neverAcceptCases)
+  });
+
+  testRule({
+    ruleName,
+    config: ["never"],
+    ...options,
+    skipBasicChecks: true,
+    reject: withSyntaxLabel(syntaxLabel, neverRejectCases)
+  });
+}
